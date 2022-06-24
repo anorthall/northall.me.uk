37d4a569cf45281064de8e1a2c876da212058ae4
Andrew Northall <andrew@northall.me.uk>
2022-06-24 10:20:04 +0100

    Make output more readable

--- build_site_and_copy.sh
+++ build_site_and_copy.sh
@@ -3,10 +3,12 @@
 WEB_ROOT=/home/andrew/www/northall.me.uk/www
 
 # Update from git
+#echo ""
 #echo "Pulling from git..."
 #git pull
 
 # Build simple-photo-gallery
+echo ""
 echo "Building photo gallery..."
 
 cd gallery
@@ -15,23 +17,30 @@ gallery-build
 deactivate
 cd ..
 
+echo ""
 echo "Copying photo gallery to Jekyll..."
 cp gallery/public/index.html www/photos.html
 
 # Build Jekyll
+echo ""
 echo "Building Jekyll..."
 cd www
 bundle exec jekyll build
 
 # Remove old files
+echo ""
 echo "Removing old files from web root..."
 rm -r $WEB_ROOT/*
 
 # Copy Jekyll files
+echo ""
 echo "Copying Jekyll generated files to web root..."
 cp -r _site/* $WEB_ROOT
 
 # Copy gallery images
+echo ""
 echo "Copying photo gallery images to web root..."
 cp -r ../gallery/public/images $WEB_ROOT/images/gallery
 
+echo ""
+echo "northall.me.uk completed."
