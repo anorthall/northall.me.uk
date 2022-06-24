641e8a3811ac59ab6b3abcab306a84986d73f36d
Andrew Northall <andrew@northall.me.uk>
2022-06-24 10:18:14 +0100

    Make build script less verbose

--- build_site_and_copy.sh
+++ build_site_and_copy.sh
@@ -3,26 +3,35 @@
 WEB_ROOT=/home/andrew/www/northall.me.uk/www
 
 # Update from git
+#echo "Pulling from git..."
 #git pull
 
 # Build simple-photo-gallery
+echo "Building photo gallery..."
+
 cd gallery
 source ./venv/bin/activate
 gallery-build
 deactivate
 cd ..
-cp -v gallery/public/index.html www/photos.html
+
+echo "Copying photo gallery to Jekyll..."
+cp gallery/public/index.html www/photos.html
 
 # Build Jekyll
+echo "Building Jekyll..."
 cd www
 bundle exec jekyll build
 
 # Remove old files
-rm -rfv $WEB_ROOT/*
+echo "Removing old files from web root..."
+rm -r $WEB_ROOT/*
 
 # Copy Jekyll files
-cp -rfv _site/* $WEB_ROOT
+echo "Copying Jekyll generated files to web root..."
+cp -r _site/* $WEB_ROOT
 
 # Copy gallery images
+echo "Copying photo gallery images to web root..."
 cp -rfv ../gallery/public/images $WEB_ROOT/images/gallery
 
